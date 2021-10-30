b377f7bd5dc6c384b7a79915fd2b1fa13bc2e79b
Cesar Leon <leoncesaralejandro@gmail.com>
2021-10-30 23:14:26 +0000

    Updated to the last version

--- restart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-killall nm-applet
-if [ -x /usr/bin/nm-applet ]; then
-    nm-applet &
-fi
--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-emacs --daemon &
-emacsclient -c -a 'emacs' &
-brave &
-nitrogen --restore &
-picom --config $HOME/.config/picom/picom.conf &
-if [ -x /usr/bin/nm-applet ]; then
-    nm-applet &
-fi
